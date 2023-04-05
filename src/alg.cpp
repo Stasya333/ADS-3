fd7d6ca2e4bf887b967b531209a38ade024b08ff
Titova-Anastasiya <110681363+Stasya333@users.noreply.github.com>
2023-04-05 13:30:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0, right = size - 1, count = 0;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (arr[mid] == value) {
+      count++;
+      for (int i = mid - 1; i >= left; i--) {
+        if (arr[i] == value) {
+          count++;
+        } else {
+          break;
+        }
+      }
+      for (int i = mid + 1; i <= right; i++) {
+        if (arr[i] == value) {
+          count++;
+        } else {
+          break;
+        }
+      }
+      return count;
+    } else if (arr[mid] < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return 0;
 }
